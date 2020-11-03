4a55da9d538f44f31af0cc857a90453833ce15d7
gdulik <tomas.gdula@gmail.com>
2020-11-03 09:45:43 +0100

    CRUD implement

--- 09_Express/04_REST/index.js
+++ 09_Express/04_REST/index.js
@@ -3,14 +3,16 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
+const methodOverride = require('method-override');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method'));
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'))
 
-const comments = [
+let comments = [
   {
     id: uuidv4(),
     username: 'Tod',
@@ -57,6 +59,28 @@ app.get('/comments/:id', (req, res) => {
   res.render('comments/show', { comment });
 })
 
+// PATCH /comments/:id - update one comment
+app.get('/comments/:id/edit', (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find(c => c.id === id)
+  res.render('comments/edit', { comment });
+})
+
+app.patch('/comments/:id', (req, res) => {
+  const { id } = req.params;
+  const newCommentText = req.body.comment;
+  const foundComment = comments.find(c => c.id === id);
+  foundComment.comment = newCommentText;
+  res.redirect('/comments');
+})
+
+// DELETE /comments/:id - remove one comment
+app.delete('/comments/:id', (req, res) => {
+  const { id } = req.params;
+  comments = comments.filter(c => c.id !== id);
+  res.redirect('/comments');
+})
+
 app.get('/tacos', (req, res) => {
   res.send('GET /tacos response');
 })
@@ -69,9 +93,3 @@ app.post('/tacos', (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 })
-
-
-
-
-// PATCH /comments/:id - update one comment
-// DELETE /comments/:id - remove one comment
