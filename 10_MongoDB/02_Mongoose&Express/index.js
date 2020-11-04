19325e2452c67e6753e3b8fc8c669826a87d51f4
gdulik <tomas.gdula@gmail.com>
2020-11-04 11:34:52 +0100

    farmStand project

--- 10_MongoDB/02_Mongoose&Express/index.js
+++ 10_MongoDB/02_Mongoose&Express/index.js
@@ -1,2 +1,80 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const Product = require('./models/product');
+const methodOverride = require('method-override')
+
+mongoose.connect('mongodb://localhost:27017/farmStand', {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => {
+    console.log('Mongo connection open');
+  })
+  .catch(err => {
+    console.log('Oh no, Mongo error occured');
+    console.log(err);
+  })
+
+app.set('views',path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+const categories = ['fruit','vegetable','dairy'];
+
+// View all products
+app.get('/products', async (req, res) => {
+  const { category } = req.query;
+  if(category){
+    const products = await Product.find({ category: category });
+    res.render('products/index', { products, category });
+  } else {
+    const products = await Product.find({});
+    res.render('products/index', { products, category: 'All' });
+  }
+  
+})
+
+// Create new product
+app.get('/products/new', (req, res) => {
+  res.render('products/new', { categories });
+})
+
+app.post('/products', async (req, res) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  res.redirect(`/products/${newProduct._id}`);
+})
+
+// View product
+app.get('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render('products/show', { product });
+})
+
+// Edit product
+app.get('/products/:id/edit', async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render('products/edit', { product, categories });
+})
+
+app.put('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+  res.redirect(`/products/${product._id}`);
+})
+
+// Delete product
+app.delete('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  const deletedProduct = await Product.findByIdAndDelete(id);
+  res.redirect('/products');
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+})
